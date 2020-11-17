edbcac3ab998b13606a2137638f7aed1958b00ab
Hana <hana@Hanaui-MacBookPro.local>
2020-11-17 12:20:56 +0900

    Todos : data binding - from parent to child

--- src/app/todo/todos/todo/todo.component.ts
+++ src/app/todo/todos/todo/todo.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../../share/todo.model';
 
 @Component({
   selector: 'app-todo',
   template: `
-      <input type="checkbox"> 운동하기
+    <input type="checkbox" [checked]="todo.done"> {{ todo.text }}
   `,
   styles: [
   ]
 })
 export class TodoComponent implements OnInit {
+
+  @Input() todo: Todo;
 
   constructor() { }
 
