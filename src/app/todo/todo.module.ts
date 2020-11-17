071a8b15a98b7a668e4f34a31651d794b8d12645
Hana <hana@Hanaui-MacBookPro.local>
2020-11-17 11:35:22 +0900

    Todos : add-todo

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodosComponent } from './todos/todos.component';
 import { TodoComponent } from './todos/todo/todo.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [TodosComponent, TodoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [TodosComponent]
 })
--- src/app/todo/todos/todos.component.ts
+++ src/app/todo/todos/todos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+  newText: '';
   todos: {
     done: boolean,
     text: string
@@ -14,8 +14,8 @@ export class TodosComponent implements OnInit {
 
   constructor() { 
     this.todos = [
-      { done:false, text: '운동하기' },
-      { done:true, text: '공부하기' }
+      { done: false, text: '운동하기' },
+      { done: true, text: '공부하기' }
     ]
    }
 
@@ -25,5 +25,12 @@ export class TodosComponent implements OnInit {
   toggleTodo(todo) {
     todo.done = !todo.done
   }
-
+  
+  addTodo(newText: string) {
+    this.todos.push({
+      done: false, 
+      text: newText
+    });
+    this.newText = '';
+  }
 }
